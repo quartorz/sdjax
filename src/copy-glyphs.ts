f7d7b65ea833fb2c16d8543d4b4008e163c0818b
quartorz <nakayama-daisuke@kayac.com>
2020-11-19 12:41:47 +0900

    transformじゃなくてvisibilityを使う

--- src/copy-glyphs.ts
+++ src/copy-glyphs.ts
@@ -1,6 +1,6 @@
 const copyGlyphs = (glyphSource: SVGSVGElement, svg: SVGSVGElement) => {
   const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-  g.setAttribute('transform', 'scale(0,0)');
+  g.setAttribute('visibility', 'hidden');
 
   for (const n of glyphSource.firstChild.childNodes) {
     console.log(n);
