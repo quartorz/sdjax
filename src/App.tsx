3ecba9b4a8a114ef8dc19988cd1f75dd525eef25
quartorz <nakayama-daisuke@kayac.com>
2021-01-21 10:36:36 +0900

    SVGの出力を修正

--- src/App.tsx
+++ src/App.tsx
@@ -41,35 +41,22 @@ const App: React.FC<{}> = () => {
 
             copyGlyphs(glyphs, svg);
 
-            //get svg source.
             const serializer = new XMLSerializer();
-            let source = serializer.serializeToString(svg);
+            const source = `\
+<?xml version="1.0" standalone="yes"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+${serializer.serializeToString(svg)}`;
 
-            //add name spaces.
-            if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-                source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-            }
-            if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-                source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-            }
-
-            //add xml declaration
-            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-            //convert svg source to URI data scheme.
-            var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-            //set url value to a element's href attribute.
+            const url = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(source);
             const a = document.createElement("a")
             a.href = url;
             a.download = 'Output.svg';
             a.innerText = 'a';
             document.body.appendChild(a);
             a.click();
-            //you can download svg file by right click menu.
+
             setTimeout(() => {
               document.body.removeChild(a);
-              URL.revokeObjectURL(url);
             }, 0);
           }}
         >Save</button>
